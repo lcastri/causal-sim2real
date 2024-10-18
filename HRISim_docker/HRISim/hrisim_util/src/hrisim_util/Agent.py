d5af50a2f3f467aaa78cb43914aefab043215a2a
Luca Castri <lucacastri94@gmail.com>
2024-10-18 12:47:18 +0100

    working

--- HRISim_docker/HRISim/hrisim_util/src/hrisim_util/Agent.py
+++ HRISim_docker/HRISim/hrisim_util/src/hrisim_util/Agent.py
@@ -4,7 +4,8 @@ import networkx as nx
 from scipy import stats
 from geometry_msgs.msg import Point
 import numpy as np
-import rospy
+
+DEFAULT_VALUE = -1000
 
 class Agent:
     def __init__(self, id, schedule, graph, allowTask, maxTaskTime) -> None:
@@ -57,13 +58,58 @@ class Agent:
         self.pastDest = self.original_path[max(0, self.original_path.index(destname) - 1)]
         self.currDest = self.original_path[self.original_path.index(destname)]
         self.nextDest = self.original_path[min(len(self.original_path) - 1, self.original_path.index(destname) + 1)]
-        self.nextDestPos = dest
+        self.nextDestPos = [dest.x, dest.y, 0]
         
         return destname, dest
     
     @property
     def finalDest(self):
         return self.original_path[-1] if self.original_path else None
+    
+       
+    def to_dict(self):
+        """Serialize the Agent object to a dictionary, replacing None values with defaults."""
+        return {
+            'id': self.id,
+            'x': self.x if self.x is not None else DEFAULT_VALUE,
+            'y': self.y if self.y is not None else DEFAULT_VALUE,
+            'path': self.path if self.path is not None else [],
+            'original_path': self.original_path if self.original_path is not None else [],
+            'pastDest': self.pastDest if self.pastDest is not None else '',
+            'pastFinalDest': self.pastFinalDest if self.pastFinalDest is not None else '',
+            'currDest': self.currDest if self.currDest is not None else '',
+            'nextDest': self.nextDest if self.nextDest is not None else '',
+            'nextDestPos': self.nextDestPos if self.nextDestPos is not None else [DEFAULT_VALUE, DEFAULT_VALUE, DEFAULT_VALUE],
+            'nextDestRadius': self.nextDestRadius if self.nextDestRadius is not None else DEFAULT_VALUE,
+            'startingTime': self.startingTime if self.startingTime is not None else DEFAULT_VALUE,
+            'exitTime': self.exitTime if self.exitTime is not None else DEFAULT_VALUE,
+            'atWork': self.atWork,
+            'isQuitting': self.isQuitting,
+            'isStuck': self.isStuck,
+            'taskDuration': self.taskDuration if self.taskDuration is not None else DEFAULT_VALUE
+        }
+
+    @classmethod
+    def from_dict(cls, data, schedule, graph, allowTask, maxTaskTime):
+        """Deserialize a dictionary to an Agent object."""
+        agent = cls(data['id'], schedule, graph, allowTask, maxTaskTime)
+        agent.x = data['x'] if data['x'] != DEFAULT_VALUE else None
+        agent.y = data['y'] if data['y'] != DEFAULT_VALUE else None
+        agent.path = data['path']
+        agent.original_path = data['original_path'] if data['original_path'] != [] else None
+        agent.pastDest = data['pastDest'] if data['pastDest'] != '' else None
+        agent.pastFinalDest = data['pastFinalDest'] if data['pastFinalDest'] != '' else None
+        agent.currDest = data['currDest'] if data['currDest'] != '' else None
+        agent.nextDest = data['nextDest'] if data['nextDest'] != '' else None
+        agent.nextDestPos = data['nextDestPos'] if data['nextDestPos'] != [DEFAULT_VALUE, DEFAULT_VALUE] else None
+        agent.nextDestRadius = data['nextDestRadius'] if data['nextDestRadius'] != DEFAULT_VALUE else None
+        agent.startingTime = data['startingTime'] if data['startingTime'] != DEFAULT_VALUE else None
+        agent.exitTime = data['exitTime'] if data['exitTime'] != DEFAULT_VALUE else None
+        agent.atWork = data['atWork']
+        agent.isQuitting = data['isQuitting']
+        agent.isStuck = data['isStuck']
+        agent.taskDuration = data['taskDuration'] if data['taskDuration'] != DEFAULT_VALUE else None
+        return agent
     
     def heuristic(self, a, b):
         pos = nx.get_node_attributes(self.G, 'pos')
--- HRISim_docker/HRISim/peopleflow/peopleflow_pedsim_bridge/scripts/PedsimBridge.py
+++ HRISim_docker/HRISim/peopleflow/peopleflow_pedsim_bridge/scripts/PedsimBridge.py
@@ -1,17 +1,14 @@
 #!/usr/bin/env python
 
+import rospy
 import copy
 import random
-
-import rospy
 import pickle
 from pedsim_srvs.srv import GetNextDestination, GetNextDestinationResponse
-from Agent import Agent
-from std_msgs.msg import  Header, String
-from geometry_msgs.msg import Point
-from peopleflow_msgs.msg import pfAgents, pfAgent, Time as pT
-import traceback
+from peopleflow_msgs.msg import Time as pT
+from hrisim_util.Agent import Agent 
 import hrisim_util.ros_utils as ros_utils
+import traceback
 import time
 
 SHELFS = ["shelf1", "shelf2", "shelf3", "shelf4", "shelf5", "shelf6"]
@@ -22,11 +19,11 @@ def seconds_to_hhmmss(seconds):
 
 class PedsimBridge():
     def __init__(self):
+        self.load_agents()
+
         rospy.Subscriber("/peopleflow/time", pT, self.cb_time)
         self.timeOfDay = None
-        self.agents = {}
-        self.agents_pub = rospy.Publisher('/peopleflow/agents', pfAgents, queue_size=10)
-
+        
         rospy.Service('get_next_destination', GetNextDestination, self.handle_get_next_destination)
         rospy.loginfo('ROS service /get_next_destination advertised')
         
@@ -35,18 +32,35 @@ class PedsimBridge():
         self.elapsedTimeString = t.hhmmss.data
         self.elapsedTime = t.elapsed
         
-    def get_agent(self, req) -> Agent:
-        # Retrieving agent info
-        if req.agent_id not in self.agents:
-            self.agents[req.agent_id] = Agent(req.agent_id, copy.deepcopy(SCHEDULE), G, ALLOW_TASK, MAX_TASKTIME)
-        self.agents[req.agent_id].x = req.origin.x
-        self.agents[req.agent_id].y = req.origin.y
-        self.agents[req.agent_id].isStuck = req.is_stuck
+    def load_agents(self):
+        """Load agents from the ROS parameter server."""
+        self.agents = {}
+        agents_param = rospy.get_param('/peopleflow/server/agents', None)
+        if agents_param is not None:
+            self.agents = {agent_id: Agent.from_dict(agent_data, SCHEDULE, G, ALLOW_TASK, MAX_TASKTIME) for agent_id, agent_data in agents_param.items()}
+    
+    def save_agents(self):
+        """Save agents to the ROS parameter server."""
+        agents_dict = {str(agent_id): agent.to_dict() for agent_id, agent in self.agents.items()}
+        rospy.set_param('/peopleflow/server/agents', agents_dict)
         
-        return self.agents[req.agent_id]
+    def get_agent(self, req) -> Agent:
+        agent_id = str(req.agent_id)
+        
+        # Retrieving agent info
+        if agent_id not in self.agents:
+            self.agents[agent_id] = Agent(agent_id, copy.deepcopy(SCHEDULE), G, ALLOW_TASK, MAX_TASKTIME)
+        self.agents[agent_id].x = req.origin.x
+        self.agents[agent_id].y = req.origin.y
+        self.agents[agent_id].isStuck = req.is_stuck
+        
+        return self.agents[agent_id]
      
     def handle_get_next_destination(self, req):
         try:
+            # Load agents from rosparam
+            self.load_agents()
+            
             agent = self.get_agent(req)
             
             # Entrance logic
@@ -60,7 +74,6 @@ class PedsimBridge():
                 #   task_duration = random.randint(0, SCHEDULE['starting']['duration'] - 10) 
                 # ! this agent won't ask again a destination for "task_duration" seconds
                 if agent.startingTime is None:
-                    rospy.logwarn(f"{agent.id} ELIF 1.1")
                     agent.startingTime = random.randint(self.elapsedTime, SCHEDULE['starting']['duration'] - 10)
                     agent.exitTime = int(sum([SCHEDULE[t]['duration'] for t in SCHEDULE if t in ['starting','morning','lunch','afternoon']]) + agent.startingTime)
                     agent.setTask('parking', agent.startingTime)
@@ -70,8 +83,7 @@ class PedsimBridge():
                 #   dest = agent.selectDestination(self.timeOfDay, req.destinations), 
                 #   task_duration = 0 
                 # ! atWork = True --> this agent won't enter again this if
-                elif agent.startingTime is not None:
-                    rospy.logwarn(f"{agent.id} ELIF 1.2")
+                else:
                     next_destination = agent.selectDestination(self.timeOfDay, req.destinations)                           
                     agent.setTask(next_destination, agent.getTaskDuration())
                     agent.atWork = True
@@ -85,30 +97,24 @@ class PedsimBridge():
                   agent.atWork and agent.isFree and not agent.isQuitting and
                   self.elapsedTime >= agent.exitTime):
                 
-                rospy.logwarn(f"{agent.id} ELIF 2")
                 agent.setTask('parking', SCHEDULE['quitting']['duration'] - agent.startingTime + SCHEDULE['off']['duration'])
                 agent.isQuitting = True
     
             # New goal logic                
-                
             elif agent.atWork and agent.isStuck:
-                rospy.logwarn(f"{agent.id} ELIF 3")
                 next_destination = agent.selectDestination(self.timeOfDay, req.destinations) if not agent.isQuitting else 'parking'
                 agent.setTask(next_destination)
                 rospy.logerr(f"Agent {agent.id} is stuck. new desination: {next_destination}")
                         
             elif agent.atWork and not agent.isStuck and agent.isQuitting and len(agent.path) == 1:
-                rospy.logwarn(f"{agent.id} ELIF 4")
                 agent.atWork = False
                 agent.isQuitting = False
                 
             elif agent.isFree and agent.atWork and not agent.isStuck and not agent.isQuitting:
-                rospy.logwarn(f"{agent.id} ELIF 5")
                 next_destination = agent.selectDestination(self.timeOfDay, req.destinations)
                 agent.setTask(next_destination, agent.getTaskDuration())
                 
             elif not agent.isFree:
-                rospy.logwarn(f"{agent.id} ELIF 6")
                 pass
                 
             else:
@@ -124,51 +130,23 @@ class PedsimBridge():
                 rospy.logerr(f"isStuck {agent.isStuck}")
                 rospy.logerr(f"closestWP {agent.closestWP}")
                 
-                    
+            
             # Response
             wpname, wp = agent.nextWP         
             agent.nextDestRadius = WPS[wpname]["r"] if wpname in WPS else 1.0
-            rospy.logwarn(f"{agent.id} RESPONSE")
-            rospy.logwarn(f"---- {wpname}")
-            rospy.logwarn(f"---- {agent.taskDuration[wpname]}")
             response = GetNextDestinationResponse(destination_id=wpname, 
                                                   destination=wp, 
                                                   destination_radius=WPS[wpname]["r"] if wpname in WPS else 1.0,
                                                   task_duration=agent.taskDuration[wpname])
+            # Save agents to rosparam
+            self.save_agents()
             return response
         
         except Exception as e:
             rospy.logerr(f"Time: {self.timeOfDay} - {self.elapsedTimeString}")
             rospy.logerr(f"Agent {agent.id} generated error: {str(e)}")
             rospy.logerr(f"Traceback: {traceback.format_exc()}")
-    
-    
-    def pub_agents(self):
-        msg_Agents = pfAgents()
-        msg_Agents.header = Header()
-            
-        for a in self.agents.values():
-            msg_Agent = pfAgent()
-            msg_Agent.header = Header()
-            msg_Agent.id = a.id
-            msg_Agent.starting_time = String(data=seconds_to_hhmmss(TIME_INIT*3600 + a.startingTime) if a.startingTime is not None else "")
-            msg_Agent.exit_time = String(data=seconds_to_hhmmss(TIME_INIT*3600 + a.exitTime) if a.exitTime is not None else "")
-            msg_Agent.position = Point(a.x, a.y, 0)
-            msg_Agent.is_stuck.data = a.isStuck
-            msg_Agent.at_work.data = a.atWork
-            msg_Agent.is_quitting.data = a.isQuitting
-            msg_Agent.past_WP_id = String(data=a.pastDest if a.pastDest is not None else "")
-            msg_Agent.current_WP_id = String(data=a.currDest if a.currDest is not None else "")
-            msg_Agent.path = [String(data=wp or "") for wp in a.path]
-            msg_Agent.next_WP_id = String(data=a.nextDest if a.nextDest is not None else "")
-            msg_Agent.next_destination = a.nextDestPos if a.nextDestPos is not None else Point(0,0,0)
-            msg_Agent.next_destination_radius = a.nextDestRadius if a.nextDestPos is not None else 0
-            msg_Agent.final_WP_id = String(data=a.finalDest if a.finalDest is not None else "")
-            msg_Agent.task_duration = a.taskDuration[a.nextDest] if a.nextDest is not None and a.taskDuration[a.nextDest] is not None else 0
-
-            msg_Agents.agents.append(msg_Agent)
-        self.agents_pub.publish(msg_Agents)
-    
+       
   
 if __name__ == '__main__':
     rospy.init_node('peopleflow_pedsim_bridge')
@@ -186,8 +164,4 @@ if __name__ == '__main__':
         
     pedsimBridge = PedsimBridge()
                 
-    while not rospy.is_shutdown():
-        
-        pedsimBridge.pub_agents()
-               
-        rate.sleep()
+    rospy.spin()
